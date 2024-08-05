db86c159cdfdc8dbaaaffeddf530d5d32194fb71
Sten <uuesoo.sten@outlook.com>
2024-08-05 19:18:47 +0300

    Removed cypress folder

--- cypress/support/index.js
+++
@@ -1,6 +0,0 @@
-// cypress/support/index.js
-Cypress.on('uncaught:exception', (err, runnable) => {
-    // returning false here prevents Cypress from
-    // failing the test
-    return false;
-  });
