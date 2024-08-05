8d2a6180f5abd16851497872e799466bf15a4725
Sten <uuesoo.sten@outlook.com>
2024-08-05 19:11:08 +0300

    production build succesful

--- my-nextjs-todo-app/app/components/todoTodo.cy.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import Todo from './todo'
-
-describe('<Todo />', () => {
-  it('renders', () => {
-    // see: https://on.cypress.io/mounting-react
-    cy.mount(<Todo />)
-  })
-})
